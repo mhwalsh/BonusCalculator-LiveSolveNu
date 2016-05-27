46522b9b3e3acafc39bdd0ec8cc33b1d683f6b2b
Millie Walsh <millie@primeacademy.io>
2016-05-27 11:47:55 -0500

    rating bonus calc

--- group_01.js
+++ group_01.js
@@ -1,3 +1,5 @@
+
+// [Name, emp num, sal, rating]
 var atticus = ["Atticus", "2405", "47000", 3];
 var jem = ["Jem", "62347", "63500", 4];
 var boo = ["Boo", "11435", "54000", 3];
@@ -5,8 +7,43 @@ var scout = ["Scout", "6243", "74750", 5];
 
 var employees = [atticus, jem, boo, scout];
 
+// calcBonus take an individual array and outputs new bonus array
 function calcBonus(individual){
-  console.log(individual[0]);
+  //marshal vars
+  var empName = individual[0];
+  var empNumber = individual[1];
+  var empSalary = individual[2];
+  var empRating = individual[3];
+
+  console.log('Processing ', empName);
+
+  //in decimals
+  var bonusPercentage = 0.0;
+  //rating calc
+  switch (empRating) {
+    //assuming no negs
+    case 0:
+    case 1:
+    case 2:
+      bonusPercentage = 0.0;
+      break;
+    case 3:
+      bonusPercentage = 0.04;
+      break;
+    case 4:
+      bonusPercentage = 0.06;
+      break;
+    case 5:
+      bonusPercentage = 0.10;
+      break;
+    default:
+      console.log('Not a possible rating.');
+      bonusPercentage = 0.0;
+  }
+
+  console.log('bonusPercentage - ', bonusPercentage);
+// [name, percent, adjusted comp, bonus]
+//return
 }
 
 for (var i = 0; i < employees.length; i++) {
