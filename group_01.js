c39c0a54c84eab026217f14a151f3dbfe4244f8d
Millie Walsh <millie@primeacademy.io>
2016-05-27 14:29:41 -0500

    clean up docs and format

--- group_01.js
+++ group_01.js
@@ -1,5 +1,11 @@
+/**
+  Prime - Nu Live Solve of the Group Bonus Challenge.
+  5/27/16 - Millie Walsh
+**/
 
-// [Name, emp num, sal, rating]
+//-- Data - Global State --//
+// individual employee arrary formatted
+// [name , number, salary, rating]
 var atticus = ["Atticus", "2405", "47000", 3];
 var jem = ["Jem", "62347", "63500", 4];
 var boo = ["Boo", "11435", "54000", 3];
@@ -7,9 +13,14 @@ var scout = ["Scout", "6243", "74750", 5];
 
 var employees = [atticus, jem, boo, scout];
 
-// calcBonus take an individual array and outputs new bonus array
+// -- Helper Funtions -- //
+
+/**
+  function calcBonus takes an individual array
+  and outputs new array of bonus information.
+**/
 function calcBonus(individual){
-  //marshal vars
+  //marshal variables and give them useful names
   var empName = individual[0];
   var empNumber = individual[1];
   var empSalary = Number(individual[2]);
@@ -17,11 +28,11 @@ function calcBonus(individual){
 
   console.log('Processing ', empName);
 
-  //in decimals
-  var bonusPercentage = 0.0;
-  //rating calc
+  var bonusPercentage; //in decimals
+
+  //rating calculation
   switch (empRating) {
-    //assuming no negs
+    //assuming rating from 0-5, no negatives
     case 0:
     case 1:
     case 2:
@@ -41,29 +52,39 @@ function calcBonus(individual){
       bonusPercentage = 0.0;
   }
 
-  //employee number <= 4
+  //employee number <= 4, get additional 5%
   if (empNumber.length <= 4) {
     bonusPercentage += 0.05;
   }
 
-  //salary greater than 65000
+  //salary > than 65000, get 1% less
   if(empSalary > 65000){
     bonusPercentage -= 0.01;
   }
 
-  //no bonus above 13%
+  //no bonus can be above 13%
   if(bonusPercentage > 0.13){
     bonusPercentage = 0.13;
   }
 
-  // [name, percent, adjusted comp, bonus]
   var bonus = Math.round(empSalary * bonusPercentage);
   var adjustedComp = empSalary + bonus;
 
-  var newEmpArr = [empName, bonusPercentage, adjustedComp, bonus];
+  // build final array to be returned
+  // [name, percent, adjusted comp, bonus]
+  var newEmpArr = [empName, bonusPercentage.toLocaleString('percent', {style: 'percent'}), localeDollars(adjustedComp), localeDollars(bonus)];
   return newEmpArr;
 }
 
+/**
+function localeDollars takes a number and converts
+it to a USD representation as a string.
+**/
+function localeDollars(dollar){
+  return dollar.toLocaleString('USD', {style: 'currency', currency: 'USD'});
+}
+
+// -- Main Execution -- //
 for (var i = 0; i < employees.length; i++) {
   console.log(calcBonus(employees[i]));
 }
